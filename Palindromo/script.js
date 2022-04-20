4c25dbe08534cc2eefea90491c58d77e9a929f04
SebastianoMV <sebastianovisco@gmail.com>
2022-04-20 15:49:48 +0200

    Palindromi con input

--- Palindromo/script.js
+++ Palindromo/script.js
@@ -2,19 +2,23 @@
 //Creare una funzione per capire se la parola inserita è palindroma
 // Bonus: L’inserimento avviene tramite un campo input
 
-const parola = prompt('inserisci una parola');
+const btnparola = document.querySelector('#btn-parola');
 
-function reverse(parolaInserita){
-  let parolaAlContrario = '';
-  for (let i = parolaInserita.length - 1; i >= 0; i--) { 
-    parolaAlContrario += parolaInserita[i];
-  }
+btnparola.addEventListener('click', function(){
+  const parola = document.getElementById('parola').value;
+
+  function reverse(parolaInserita){
+   let parolaAlContrario = '';
+    for (let i = parolaInserita.length - 1; i >= 0; i--) { 
+     parolaAlContrario += parolaInserita[i];
+   }
   
-  return parolaAlContrario;
-}
+    return parolaAlContrario;
+  }
 
-if( parola == reverse(parola)){
-  document.querySelector('h1').innerHTML = `${parola} è una parola palindroma`
-}else{
-  document.querySelector('h1').innerHTML = `${parola} NON è una parola palindroma`
-}
+  if( parola == reverse(parola)){
+    document.querySelector('h1').innerHTML = `${parola} è una parola palindroma`
+  }else{
+     document.querySelector('h1').innerHTML = `${parola} NON è una parola palindroma`
+  } 
+})
